37466278278fff0b4f32c5c33c894b680fc11f21
Jannik <jannmann1@web.de>
2017-11-05 21:58:01 +0100

    fixed error ocurred at building output directory

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcwV5oiyvA1_lwQUBhT4vkIesauYk2Jfk",
+    authDomain: "seemann-jannik.firebaseapp.com",
+    databaseURL: "https://seemann-jannik.firebaseio.com",
+    projectId: "seemann-jannik",
+    storageBucket: "seemann-jannik.appspot.com",
+  }
 };
