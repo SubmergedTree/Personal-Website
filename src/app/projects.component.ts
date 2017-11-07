9c543539a768d594717dd8e42b825bc45f1cfbf2
Jannik <jannmann1@web.de>
2017-11-07 15:24:50 +0100

    added loading sign for projects list

--- src/app/projects.component.ts
+++ src/app/projects.component.ts
@@ -29,11 +29,12 @@ const PROJECTS = [
 })
 export class ProjectsComponent {
   public items: any[];
+  public isLoading = true;
 
   constructor(db: AngularFireDatabase){
     db.list('/items').valueChanges().subscribe(items => {
       this.items = items;
-      console.log(this.items);
+      this.isLoading = false;
     });
   }
 
